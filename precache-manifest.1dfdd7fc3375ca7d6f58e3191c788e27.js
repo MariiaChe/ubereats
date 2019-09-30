d1fdab211fe53f2cdba5f206afd86a525c288bbc
Mariia <maria.bookvoed@gmail.com>
2019-09-30 14:50:14 -0300

    Updates

---
+++ precache-manifest.1dfdd7fc3375ca7d6f58e3191c788e27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb6dd0111bb0b97ce53218b63c44cca",
+    "url": "/ubereats/index.html"
+  },
+  {
+    "revision": "c58eacd09a8c2fd50457",
+    "url": "/ubereats/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d2389509a599a4067acd",
+    "url": "/ubereats/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c58eacd09a8c2fd50457",
+    "url": "/ubereats/static/js/2.e8b9a0f6.chunk.js"
+  },
+  {
+    "revision": "d2389509a599a4067acd",
+    "url": "/ubereats/static/js/main.d906427f.chunk.js"
+  },
+  {
+    "revision": "8aaed043052bf5b7f515",
+    "url": "/ubereats/static/js/runtime-main.f25c727e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ubereats/static/media/logo.25bf045c.svg"
+  }
+]);
