073f50273c42a8ecc93850d9554afc0bf9f48645
luisaromero <luisa.marcela.romero@gmail.com>
2019-10-02 14:44:40 +0100

    slider

--- ubereats/src/Components/ScrollingNavbar.jsx
+++ ubereats/src/Components/ScrollingNavbar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './ScrollingNavbar.css'
+import Supermarket from '../supermarket.json'
+
 
 class ScrollingNavbar extends Component{
     render(){
@@ -7,13 +9,7 @@ class ScrollingNavbar extends Component{
             <div class="container">
             <nav class="menu-wrapper">
               <ul class="menu">
-                <li><a href="#">Item One</a></li>
-                <li><a href="#">Item Two</a></li>
-                <li><a href="#">Item Three</a></li>
-                <li><a href="#">Item Four</a></li>
-                <li><a href="#">Item Five</a></li>
-                <li><a href="#">Item Six</a></li>
-                <li><a href="#">Item Seven</a></li>
+               <li>{Object.keys(Supermarket.Pasillos)}</li>
               </ul>
             </nav>
             </div>
--- ubereats/src/Views/Productos.jsx
+++ ubereats/src/Views/Productos.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import NavbarTopCategories from '../Components/NavbarTopCategories'
 import ItemList from '../Components/ItemList'
+import ScrollingNavbar from '../Components/ScrollingNavbar'
+import Supermarket from '../supermarket.json'
+
 class Productos extends Component{
+    constructor(props){
+        super(props) }
     render(){
+        
         return(
             <React.Fragment>
                 <NavbarTopCategories/>
+                <ScrollingNavbar/>
                 <div className="content container">
                   <ItemList/>
                 </div>
