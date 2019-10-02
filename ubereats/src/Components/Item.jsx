c4526359f734ec30bbcf150ae2cc2757ff9c49cc
Mariia <maria.bookvoed@gmail.com>
2019-10-02 14:03:17 -0300

    Button add

--- ubereats/src/Components/Item.jsx
+++ ubereats/src/Components/Item.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Item.css'
 import HeartGray from '../img/corazon-gris.png'
 import Leche from '../img/categories/Milks/LecheColunEntera.png'
-
+import ButtonAdd from './ButtonAdd'
 class Item extends Component{
     render(){
         return(
@@ -11,9 +11,9 @@ class Item extends Component{
                 <div className="heart"><img className="img-fluid" src={HeartGray} alt="corazon gris"/></div>
                 <div className="Itemimg"><img className="img-fluid" src={Leche} alt="imagen producto"/></div>
                 <div className="Itemname"><p>Soprole - Leche Entera</p></div>
-                <div className="Itemweight"><p>1000ml</p></div>
+                {/* <div className="Itemweight"><p>1000ml</p></div> */}
                 <div className="Itemprice"><p>$900</p></div>
-                <div className="Itembutton"><button></button></div>
+                <div className="Itembutton"><ButtonAdd/></div>
               </div>    
             </React.Fragment>
         )
